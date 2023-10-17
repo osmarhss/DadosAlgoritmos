9246759608a78494beace1716e65fb7ed1b885a5
João Vitor Simão Pinheiro <joaovitor26.2002@gmail.com>
2023-10-17 15:29:09 -0300

    questão 3 finalizada.

--- Exercicio/01 - Pilha - Vetor/- 01 Pilha Vetor/exercicio3/pilha.c
+++ Exercicio/01 - Pilha - Vetor/- 01 Pilha Vetor/exercicio3/pilha.c
@@ -4,42 +4,90 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #define TAM 1
+
 #include "pilha.h"
+
 int main() {
 pilha p;
 pessoa pe;
+
 CriarPilha(&p);
+int numero_pessoa = 1;
 while(PilhaCheia(&p) == FALSO) {
-printf("\nInforme um numero inteiro: ");
+printf("\nInforme os dados da %dº pessoa: \n", numero_pessoa);
+printf("informe o CPF(no formato do cpf mesmo):");
 scanf("%s", pe.cpf);
+printf("%d", formato_cpf_valido(pe.cpf));
+while (formato_cpf_valido(pe.cpf) == FALSO)
+{
+    printf("\nCPF invalido!!\n");
+    scanf("%s", pe.cpf);
+}
+printf("\ninforme o nome:");
 scanf("%s", pe.nome);
+printf("\ninforme o endereco:");
 scanf("%s", pe.endereco);
+printf("\ninforme o telefone:");
 scanf("%s", pe.telefone);
+printf("\ninforme o celular:");
 scanf("%s", pe.celular);
+printf("\ninforme o data de nascimento:");
 scanf("%s", pe.data_de_nascimento);
+while (is_valid_date(pe.data_de_nascimento) == FALSO)
+{
+    printf("\nData invalida!!");
+    scanf("%s", pe.data_de_nascimento);
+}
+
+printf("\ninforme o date de admissao:");
 scanf("%s", pe.data_de_admisao);
+while (is_valid_date(pe.data_de_admisao) == FALSO)
+{
+    printf("\nData invalida!!");
+    scanf("%s", pe.data_de_nascimento);
+}
+printf("\ninforme o departamento:");
 scanf("%s", pe.departamento);
+printf("\ninforme o cargo:");
 scanf("%s", pe.cargo);
-scanf("%f", &pe.salario);
+printf("\ninforme o o salario:");
+scanf("%f\n", &pe.salario);
 Empilhar(&p, pe);
+numero_pessoa++;
 }
 empilharArquivo(&p, "arquivoC.dat");
-printf("testando o test");
 desempilharArquivo(&p, "arquivoC.dat");
-printf("\ndados informados: ");
+pesquisaCPF(&p);
+printf("\n\n\ndados informados: ");
+numero_pessoa = 1;
 while(PilhaVazia(&p) == FALSO) {
+printf("\nPessoa %d:", numero_pessoa);
 pe = Desempilhar(&p);
+printf("\n\nCPF:");
 printf("\n%s", pe.cpf);
+printf("\n\nnome:");
 printf("\n%s", pe.nome);
+printf("\n\nendereco:");
 printf("\n%s", pe.endereco);
+printf("\n\ntelefone:");
 printf("\n%s", pe.telefone);
+printf("\n\ncelular:");
 printf("\n%s", pe.celular);
+printf("\n\ndata de nascimento:");
 printf("\n%s", pe.data_de_nascimento);
+printf("\n\ndata de admissao:");
 printf("\n%s", pe.data_de_admisao);
+printf("\n\ndepartamento:");
 printf("\n%s", pe.departamento);
+printf("\n\ncargo:");
 printf("\n%s", pe.cargo);
+printf("\n\nsalario:");
 printf("\n%f", pe.salario);
+numero_pessoa++
 }
 printf("\n");
 system("pause");
--- Exercicio/01 - Pilha - Vetor/- 01 Pilha Vetor/exercicio3/pilha.h
+++ Exercicio/01 - Pilha - Vetor/- 01 Pilha Vetor/exercicio3/pilha.h
@@ -65,7 +65,6 @@ void empilharArquivo(pilha *p, char nome_arquivo[50]) {
 void desempilharArquivo(pilha *p, char nome_arquivo[50]) {
     int quantidade;
     FILE *arq;
-    printf("wgwhiuhrieuhbiuebiueb");
     if ((arq = fopen (nome_arquivo, "rb")) == NULL) {
         printf ("\n Erro ao abrir o arquivo arquivoC.dat.");
         system ("pause");
@@ -76,4 +75,66 @@ void desempilharArquivo(pilha *p, char nome_arquivo[50]) {
 
     printf ("Quantidade de itens lidos: %d.\n", quantidade);
     fclose (arq);
+}
+
+int formato_cpf_valido(const char *cpf) {
+    
+    if (strlen(cpf) != 14) {
+        return FALSO;
+    }
+
+
+    for (int i = 0; cpf[i] != '\0'; i++) {
+        if (i == 3 || i == 7) {
+            if (cpf[i] != '.') {
+                return FALSO;
+            }
+        } else if (i == 11) {
+            if (cpf[i] != '-') {
+                return FALSO;
+            }
+        } else if (!isdigit(cpf[i])) {
+            return FALSO;
+        }
+    }
+
+    return VERDADEIRO;
+}
+
+int is_valid_date(const char *date) {
+    int day, month, year;
+    if (sscanf(date, "%d/%d/%d", &day, &month, &year) == 3) {
+        // Verificar se o dia, mês e ano estão dentro dos limites aceitáveis
+        if (year >= 1000 && year <= 9999 &&
+            month >= 1 && month <= 12 &&
+            day >= 1 && day <= 31) {
+            // Verificar se os meses com 30 dias não têm mais de 30 dias
+            if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+                return FALSO;
+            }
+            // Verificar se fevereiro tem no máximo 29 dias em anos bissextos ou 28 em outros anos
+            else if (month == 2) {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    return day <= 29;
+                } else {
+                    return day <= 28;
+                }
+            }
+            return VERDADEIRO;
+        }
+    }
+    return FALSO;
+}
+
+void pesquisaCPF(pilha *p) {
+    char cpf[50];
+    printf("\n\n\n\n\n\n\nVamos pesquisar um CPF, informe o cpf de alguma pessoa:\n");
+    scanf("%s", cpf);
+    for(int t = 0; t<TAM; t++) {
+        if(strcmp(p->vet[t].cpf, cpf) == 0){
+            printf("\n\nA pessoa %d tem esse CPF!!", t+1);
+        }else{
+            printf("\n\nNiguem tem esse CPF!!");
+        }
+    }
 }
